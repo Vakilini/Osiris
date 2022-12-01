86198d186293b100aac0313af821dc24c9b0d98f
Vakilini <vani.vakulenko@yandex.ru>
2022-12-01 20:47:56 +0300

    First commit

--- Osiris_AndroidProject/Osiris_test2/app/src/main/java/com/example/osiris_test2/listeners/TestListener.java
+++
@@ -1,7 +0,0 @@
-package com.example.osiris_test2.listeners;
-
-import com.example.osiris_test2.models.Test;
-
-public interface TestListener {
-    void onTestCliced(Test test);
-}
--- Osiris_AndroidProject/Osiris_test2/app/src/main/java/com/example/osiris_test2/models/Test.java
+++
@@ -1,8 +0,0 @@
-package com.example.osiris_test2.models;
-
-import java.io.Serializable;
-
-public class Test implements Serializable {
-    public String name, description;
-    //public String [][] test;
-}
